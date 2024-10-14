f2619027e69d948b76dba521013832a9a39952cd
Sneha Mittal <snehamittal1909@gmail.com>
2024-10-14 19:20:05 +0530

    React Form Hook

--- reactForm/src/App.jsx
+++ reactForm/src/App.jsx
@@ -2,34 +2,70 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useForm } from "react-hook-form"
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors,isSubmitting},
+  } = useForm();
+
+  async function onSubmit(data){
+    //API call ko simulate krte h
+    //basically data i.e submitted will take 5s delay
+    //in that time it is allowed to perform multiple submit 
+     await new Promise((resolve)=>setTimeout(resolve,5000));
+     console.log("submitting the form ",data);
+    //by default flag comes in form i.e isSubmitting
+    //to prevent this we diasbled in input 
+  }
+
+  //we learnt applying validation
+  //apply message showing
+  //stlying on error messages
 
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>First Name:</label>
+        {/* Now form will be able to track input
+        can be validated event listener can be added */}
+        <input  
+        className={errors.firstName ? 'input-error':""}
+        {...register('firstName',
+          { required:{value:true,message:"You need to fill this field"},
+            minLength:{value:3,message:'Min Len at least 3'},
+            maxLength:{value:6,message:'Max Len at most 6'}
+          })
+        }/>
+        {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <br />
+      <div>
+        <label>Middle Name:</label>
+        <input  {...register('middleName')}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <br />
+      <div>
+        <label>Last Name:</label>
+        <input  {...register('lastName',
+          { pattern: {
+            value: /^[A-Za-z]+$/i ,
+            message:"Last name is not as per the rules"
+            }
+          })}/>
+        {errors.lastName && <p className='error-msg'>{errors.lastName.message}</p>}
+      </div>
+      <input type="submit"disabled={isSubmitting} value={isSubmitting ? "Submitting":"Submit"} />
+    </form>
   )
 }
-
+//Basic Form Handling ->create
+                      // ->fields register
+                      // -> onSubmit
+                      // -> validation
+                      // -> Error Handling
+                      // -> Styling 
+                      // ->Multiple Submit Prevention
 export default App
